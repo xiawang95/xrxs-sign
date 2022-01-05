9aef4b7d0c6c2449ae293db9741e883f6de19d5b
liaoxy <liaoxiangyun@gmail.com>
2022-01-05 18:01:54 +0800

    webhook

---
+++ utils/webhookUtil.js
@@ -0,0 +1,29 @@
+let httpUtil = require("./httpUtil")
+
+//@see Server酱 https://sct.ftqq.com/login
+const ftqq = (sckey = "", text = "") => {
+    //https://sc.ftqq.com/xx.send?text=text
+    let url = `https://sc.ftqq.com/${SCKEY}.send?text=${text}`
+    httpUtil.fetchGet(url, {}).then((r) => {
+        gl.info(r)
+    })
+}
+
+const hook = (c, text) => {
+    let {type} = c;
+    if (type === "Server酱" && c.SCKEY) {
+        ftqq(c.SCKEY, text);
+    }
+}
+
+
+module.exports = {
+    hooks(configs = [], text) {
+        for (let c of configs) {
+            try {
+                hook(c, text)
+            } catch (e) {
+            }
+        }
+    }
+}
--- workflow.js
+++ workflow.js
@@ -4,4 +4,7 @@ action.sign.sign({
     name: process.env.SIGN_NAME,
     token: process.env.SIGN_APPLET_TOKEN,
     location: process.env.SIGN_LOCATION,
+    webhook: [
+        {type: "Server酱", "SCKEY": process.env.WEBHOOK_FTQQ_SCKEY}
+    ]
 })
