a030b3a06f32e93acd2c9c022f897d1912b62704
liaoxy <liaoxiangyun@gmail.com>
2022-01-05 18:21:02 +0800

    fix

--- utils/webhookUtil.js
+++ utils/webhookUtil.js
@@ -3,7 +3,7 @@ let httpUtil = require("./httpUtil")
 //@see Server酱 https://sct.ftqq.com/login
 const ftqq = (sckey = "", text = "") => {
     //https://sc.ftqq.com/xx.send?text=text
-    let url = `https://sc.ftqq.com/${SCKEY}.send?text=${text}`
+    let url = `https://sc.ftqq.com/${sckey}.send?text=${text}`
     httpUtil.fetchGet(url, {}).then((r) => {
         gl.info(r)
     })
@@ -23,6 +23,7 @@ module.exports = {
             try {
                 hook(c, text)
             } catch (e) {
+                console.error(e)
             }
         }
     }
